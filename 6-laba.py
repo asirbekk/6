1be0e199172330a3e5c16cc7822ecaa99c00e7cd
asirbekk <78365807+asirbekk@users.noreply.github.com>
2021-03-17 04:28:17 +0300

    Add files via upload

---
+++ 6-laba.py
@@ -0,0 +1,26 @@
+#1-заданий
+# d = {'1а': 16, '1б': 22, '6а': 30, '7в': 25, '10б': 26, '11а': 33, '11б': 26 }
+
+#2-заданий
+# print(d)
+# d = {3: 'three', 2: 'two', 1: 'one'}
+# a = {'three': 3,  'two': 2, 'one': 1 }
+ 
+#3-заданий# 
+#print("What is your name? ")
+# name = str(input())
+# print("How old are your? ")
+# age = int(input())
+# print("Where are you live")
+# live = str(input())
+# print("This is ", name , "It is ", age,"(S)He live in ", live)
+# 
+#4-заданий
+#
+# d = {1: ["Python", 33.2, 'UP'],
+#      2: ["Java", 23.54, 'DOWN'],
+#      3: ["Ruby", 17.22, 'UP'],
+#      10: ["Lua", 10.55, 'DOWN'],
+#      5: ["Groovy", 9.22, 'DOWN'],
+#      6: ["C", 1.55, 'UP']
+#      }
